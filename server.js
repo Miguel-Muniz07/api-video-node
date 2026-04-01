a6817b017d7cf84a18dd5557a6eaa5f29c6d8e41
Miguel-Muniz07 <miguelmunizfeitoza@gmail.com>
2026-04-01 20:17:43 -0300

    feat: implement video CRUD routes

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ import { fastify } from 'fastify';
 
 const server = fastify();
 
+// Rotas da aplicação
+
+server.post('/videos', () => {
+    return 'Hello World!'
+});
+
+server.get('/videos', () => {
+    return 'Hello World!'
+});
+
+server.put('/videos/:id', () => {
+    return 'Hello World!'
+});
+
+server.delete('/videos/:id', () => {
+    return 'Hello World!'
+});
 server.listen({
     port: 3333
 })
